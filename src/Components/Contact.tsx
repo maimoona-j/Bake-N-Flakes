83c9ff91489da6a2e58b0445eafb6535e3b92ad8
maimoona <maimoonaj97@gmail.com>
2023-07-19 02:21:32 +0500

    create UI of home page

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -1,30 +1,108 @@
 export default function contact() {
   return (
     <>
-      <h1>contact Us</h1>
-      <div className="border-2 flex flex-col  w-2/4 p-8  ml-96 h-96 mb-7 mt-11">
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          className="border-2 mt-5 p-2"
-          placeholder="Enter your name...."
-        />
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          className="border-2  mt-5 p-2"
-          placeholder="Enter your email...."
-        />
-        <label htmlFor="name">Message</label>
-        <textarea
-        
-          className="border-2  mt-5 p-5 "
-          placeholder="Enter your feedback.."
-        >
-          {" "}
-        </textarea>
-        <button className="bg-blue-400 p-3 mt-12 w-36  ml-60  hover:bg-gray-40">Send</button>
+    <div>
+
+    </div>
+
+
+      <div>
+        <h1 className="text-xl font-bold">contact Us</h1>
+        <div className="border-2 flex flex-col  w-2/4 p-8  ml-96 h-96 mb-7 mt-11">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            className="border-2 mt-5 p-2"
+            placeholder="Enter your name...."
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            className="border-2  mt-5 p-2"
+            placeholder="Enter your email...."
+          />
+          <label htmlFor="name">Message</label>
+          <textarea
+            className="border-2  mt-5 p-5 "
+            placeholder="Enter your feedback.."
+          >
+            {" "}
+          </textarea>
+          <button className="bg-blue-400 p-3 mt-12 w-36  ml-60  hover:bg-gray-40">
+            Send
+          </button>
+        </div>
       </div>
     </>
   );
 }
+
+
+// trial
+
+// import React, { useEffect } from "react";
+// import { GoogleMap, Marker, LoadScript } from "@react-google-maps/api";
+
+// export default function Contact() {
+//   const initMap = async () => {
+//     // The location of Uluru
+//     const position = { lat: -25.344, lng: 131.031 };
+
+//     // The map, centered at Uluru
+//     const mapElement = document.getElementById("map");
+//     if (!mapElement) {
+//       return; // Exit early if the map element is not found
+//     }
+
+//     new GoogleMap(mapElement, {
+//       zoom: 4,
+//       center: position,
+//       mapId: "DEMO_MAP_ID",
+//     });
+
+//     // The marker, positioned at Uluru
+//     new Marker({ position, title: "Uluru" });
+//   };
+
+//   useEffect(() => {
+//     initMap();
+//   }, []);
+
+//   return (
+//     <>
+//       <div>
+//         <h3>My Google Maps Demo</h3>
+
+//         <div id="map" style={{ height: "400px", width: "100%" }}></div>
+//       </div>
+
+//       <div>
+//         <h1 className="text-xl font-bold">Contact Us</h1>
+//         <div className="border-2 flex flex-col w-2/4 p-8 ml-96 h-96 mb-7 mt-11">
+//           <label htmlFor="name">Name</label>
+//           <input
+//             type="text"
+//             className="border-2 mt-5 p-2"
+//             placeholder="Enter your name...."
+//           />
+//           <label htmlFor="email">Email</label>
+//           <input
+//             type="email"
+//             className="border-2 mt-5 p-2"
+//             placeholder="Enter your email...."
+//           />
+//           <label htmlFor="name">Message</label>
+//           <textarea
+//             className="border-2 mt-5 p-5"
+//             placeholder="Enter your feedback.."
+//           >
+//             {" "}
+//           </textarea>
+//           <button className="bg-blue-400 p-3 mt-12 w-36 ml-60 hover:bg-gray-40">
+//             Send
+//           </button>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
--- src/Components/CookieItem.tsx
+++ src/Components/CookieItem.tsx
@@ -1,137 +1,3 @@
-
-// code before array
-
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { useState } from "react";
-
-// export default function CookieItem() {
-//   const [cookieName, setCookieName] = useState("cookies");
-//   return (
-//     <div className="border border-solid w-64 h-96 mb-6 ml-5 bg-pink-200 m-10">
-//       <img
-//         src="https://cdn.pixabay.com/photo/2015/05/07/15/08/cookie-756601_640.jpg"
-//         alt="img"
-//         className="w-64 h-42 "
-//       />
-
-//       <div className="flex items-center text-center mt-5 mr-5">
-//         <div className="ml-auto">
-//           <h6>reviews</h6>
-//           {[1, 2, 3, 4].map((star: any) => (
-//             <FontAwesomeIcon icon="star" className="text-yellow-500   " />
-//           ))}
-//         </div>
-//       </div>
-//       <div className="m-5">
-//         <h3 className="text-lg mb-2">{cookieName}</h3>
-//         <p className="mb-2">$30</p>
-//         <button className="bg-blue-300  p-3 mb-8 w-32 hover:bg-gray-400 ">buy now</button>
-//       </div>
-//     </div>
-//   );
-// }
-
-// first try with array
-
-// import { useState } from "react";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-// interface CardProps {
-//   card: {
-//     id: number;
-//     imageSrc: string;
-//     name: string;
-//     price: string;
-//     rating: number;
-//   };
-// }
-
-// const Card: React.FC<CardProps> = ({ card }) => {
-//   const { imageSrc, name, price, rating } = card;
-
-//   return (
-//     <div className="border border-solid w-64 h-96 mb-6 ml-5 bg-pink-200 m-10">
-//       <img src={imageSrc} alt="img" className="w-64 h-42" />
-
-//       <div className="flex items-center text-center mt-5 mr-5">
-//         <div className="ml-auto">
-//           <h6>reviews</h6>
-//           {[...Array(Math.floor(rating))].map((_, index) => (
-//             <FontAwesomeIcon icon="star" className="text-yellow-500" key={index} />
-//           ))}
-//         </div>
-//       </div>
-//       <div className="m-5">
-//         <h3 className="text-lg mb-2">{name}</h3>
-//         <p className="mb-2">{price}</p>
-//         <button className="bg-blue-300 p-3 mb-8 w-32 hover:bg-gray-400">buy now</button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// const HomePage: React.FC = () => {
-//   const [bestSellers, setBestSellers] = useState([
-//     {
-//       id: 1,
-//       imageSrc:  require('../images/cake1.jpg').default,
-//       name: "Cookie 1",
-//       price: "$10",
-//       rating: 4.5,
-//     },
-//     {
-//       id: 2,
-//       imageSrc: "../images/cake2.jpg",
-//       name: "Cookie 2",
-//       price: "$15",
-//       rating: 4.2,
-//     },
-//     {
-//       id: 3,
-//       imageSrc: "../images/cake3.jpg",
-//       name: "Cookie 2",
-//       price: "$12",
-//       rating: 4.2,
-//     },
-//     {
-//       id: 4,
-//       imageSrc: "../images/cookies1.jpg",
-//       name: "Cookie 2",
-//       price: "$18",
-//       rating: 4.4,
-//     },
-//     {
-//       id: 5,
-//       imageSrc: "../images/cookies2.jpg",
-//       name: "Cookie 2",
-//       price: "$15",
-//       rating: 4.5,
-//     },
-//     {
-//       id: 6,
-//       imageSrc: "../images/cookies3.jpg",
-//       name: "Cookie 2",
-//       price: "$10",
-//       rating: 4.3,
-//     },
-//   ]);
-
-//   return (
-//     <div>
-//       {bestSellers.map((card) => (
-//         <Card key={card.id} card={card} />
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default HomePage;
-
- 
-
-
-
- 
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import cake1 from "../images/cake1.jpg";
@@ -228,92 +94,27 @@ const CookiesItem: React.FC = () => {
   ]);
 
   const limitedBestSellers = bestSellers.slice(0, 6);
+  const cakes = limitedBestSellers.slice(0, 3); // First 3 items are cakes
+  const cookies = limitedBestSellers.slice(3, 6);
 
   return (
-    <div>
-      {limitedBestSellers.map((card) => (
-        <Card key={card.id} card={card} />
-      ))}
+    <div className="flex flex-col">
+      <div className="flex flex-row">
+        {cakes.map((cake) => (
+          <div key={cake.id}>
+            <Card card={cake} />
+          </div>
+        ))}
+      </div>
+      <div className="flex flex-row">
+        {cookies.map((cookie) => (
+          <div key={cookie.id}>
+            <Card card={cookie} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
 
 export default CookiesItem;
-
-
-
-// import React, { useState } from "react";
-// import BestSeller from "./BestSeller";
-// import cake1 from "../images/cake1.jpg";
-// import cake2 from "../images/cake2.jpg";
-// import cake3 from "../images/cake3.jpg";
-// import cookies1 from "../images/cookies1.jpg";
-// import cookies2 from "../images/cookies2.jpg";
-// import cookies3 from "../images/cookies3.jpg";
-
-// interface CardData {
-//   id: number;
-//   imageSrc: string;
-//   name: string;
-//   price: string;
-//   rating: number;
-// }
-
-// const CookieItem: React.FC = () => {
-//   const [bestSellers, setBestSellers] = useState<CardData[]>([
-//     {
-//       id: 1,
-//       imageSrc: cake1,
-//       name: "Cookie 1",
-//       price: "$10",
-//       rating: 4.5,
-//     },
-//     {
-//       id: 2,
-//       imageSrc: cake2,
-//       name: "Cookie 2",
-//       price: "$15",
-//       rating: 4.2,
-//     },
-//     {
-//       id: 3,
-//       imageSrc: cake3,
-//       name: "Cookie 3",
-//       price: "$12",
-//       rating: 4.2,
-//     },
-//     {
-//       id: 4,
-//       imageSrc: cookies1,
-//       name: "Cookie 4",
-//       price: "$18",
-//       rating: 4.4,
-//     },
-//     {
-//       id: 5,
-//       imageSrc: cookies2,
-//       name: "Cookie 5",
-//       price: "$15",
-//       rating: 4.5,
-//     },
-//     {
-//       id: 6,
-//       imageSrc: cookies3,
-//       name: "Cookie 6",
-//       price: "$10",
-//       rating: 4.3,
-//     },
-//   ]);
-
-//   const limitedBestSellers = bestSellers.slice(0, 6);
-
-//   return (
-//     <div>
-//       {limitedBestSellers.map((card) => (
-//         <BestSeller key={card.id} {...card} />
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default CookieItem;
